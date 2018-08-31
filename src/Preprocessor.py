0b155136df7a000edbb1e5d470a036676ec0c4ed
SeenivasanSeeni <seeni0424@gmail.com>
2018-08-31 11:47:11 +0530

    done preprocessor

--- src/Preprocessor.py
+++ src/Preprocessor.py
@@ -11,13 +11,43 @@ def makeDict(col):
             num+=1
     return d
 
-def preprocess(data):
+
+FIDict=None
+subjectsDict=None
+
+def getSubjects(data):
+
+    electives=["E1","E2","E3","E4"]
+    subjects=set()
+
+    for el in electives:
+        subjects_=list(data[el].values)
+        subjects=subjects.union(set(subjects_))
+
+    return subjects
+
+def preprocess(data,train=True):
     '''
 
     :param data: pandas dataframe containing data for preocessiong
     :return: dataframe with all the categoraicl data replaced with its numerical equivalents
     '''
-    print(data.columns)
-    import pdb
-    pdb.set_trace()
-    data.get
+    global FIDict, subjectsDict
+    #remove ID
+    data.drop(labels=["ID"],axis=1,inplace=True)
+    electives=["E1","E2","E3","E4"]
+    if(train):
+        subjects_list=getSubjects(data)
+        subjectsDict=makeDict(subjects_list)
+
+    for el in electives:
+        data[el].replace(subjectsDict,inplace=True)
+
+    if(train):
+        #find field of interest
+        FIDict=makeDict(data["FI"])
+
+    #replace FI by id
+    data["FI"].replace(FIDict,inplace=True)
+
+    return data
--- src/testPreprocessor.py
+++ src/testPreprocessor.py
@@ -5,12 +5,17 @@ def testPreProcessor():
     csvhand=han.CSVHandler("../DATA/input.csv")
     csvhand.open()
     data=csvhand.getData()
-    data_p=pr.preprocess(data)
+    data_train=pr.preprocess(data)
+    csvhand=han.CSVHandler("../DATA/input.csv")
+    csvhand.open()
+    data_test = csvhand.getData()
+    data_test=pr.preprocess(data_test,train=False)
+    import pdb
+    pdb.set_trace()
     return
 
 def testmakeDict(col):
     print(pr.makeDict(col))
 
-testmakeDict(["A","B","A","C","B"])
 
 testPreProcessor()
