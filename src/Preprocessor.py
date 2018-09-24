cfa8417ece5947c1abe713bc9db6814538ae95e2
SeenivasanSeeni <seeni0424@gmail.com>
2018-09-24 23:11:37 +0530

    done preprocessor

--- src/Preprocessor.py
+++ src/Preprocessor.py
@@ -1,72 +1,50 @@
 import pandas as pd
 
 
-subjects=[]
-fieldOfInterests=[]
-
-def makeDict(col):
-    d={}
-    num=0
-    for cell in col:
-        try:
-            d[cell]
-        except:
-            d[cell]=num
-            num+=1
-    return d
-
-
-FIDict=None
-subjectsDict=None
-
-def getSubjectsRaw(data):
-    '''
-    :param data: data consisting of tuples (FI,E1,E2,E3...EN)
-    :return: set of subjects in data
-    '''
-    subjects=set()
-    for row in data:
-        subjects=subjects.union(row[1:])
-    return subjects
-
-
-def toRaw(data_pd):
-    '''
-
-    :param data_pd: pandas dataframe
-    :return: data in raw list format
-    '''
-    data=data_pd.to_records(index=False)
-    data_list=[]
-    for d in data:
-        data_list.append(list(d))
-    return data_list
+subjects=[]  #list of subjects in the database
+fieldOfInterests=[] #list of Field of interest FI in the database
 
 
 def getSubjectsPd(data):
     '''
     :param data: pandas Dataframe consisting of tuples (FI,E1...EN)
-    :return: list of subjects
+    :return: set of subjects
     '''
-    data_r=toRaw(data)
-    subjects=getSubjectsRaw(data_r)
+
+    subjects=data.drop("FI",axis=1).values
+    subjects.resize(subjects.size)
+    subjects=set(subjects)
     return subjects
+
+def getSubjects():
+    """
+
+    :return: set of subjects that are made from train data
+    """
+    return subjects
+
+def getFI():
+    """
+
+    :return: set of field of interest(FI) made from the train data
+    """
+    return fieldOfInterests
 
 def getFIPd(data):
     """
 
-    :param data: pandas dataframe
-    :return: list of FI
+    :param data: pandas dataframe with schema (FI,E1,E2,E3...)
+    :return: set of FI
 
     """
-    fieldOfInterest=set([fi for fi in data["FI"].values])
-    return list(fieldOfInterest)
+    fieldOfInterest=set(data["FI"].values)
+    return set(fieldOfInterest)
 
 def preprocess(data,train=True):
     '''
 
-    :param data: pandas dataframe containing data for preocessiong
-    :return: dataframe with all the categorical data as separate dimension
+    :param data: pandas dataframe containing data for preprocessing
+    :return: dataframe with all the categorical data as separate dimension for each subject and for each FieldOfIntereset FI
     '''
     global subjects,fieldOfInterests
     data.drop("ID",axis=1,inplace=True)
@@ -74,8 +52,8 @@ def preprocess(data,train=True):
         subjects=getSubjectsPd(data)
         fieldOfInterests=getFIPd(data)
 
-    data_n=pd.DataFrame()
-    #make cateogical values
+    data_n=pd.DataFrame() #new dataframe
+    #make each  dimension for each value of fi
     for fi in fieldOfInterests:
         col="FI_"+fi
         values=[0]*len(data)
@@ -85,6 +63,7 @@ def preprocess(data,train=True):
                 values[fientry_i]=1
         data_n[col]=values
 
+    #make dimension for each subject possible
     for sub in subjects:
         subcol="S_"+sub
         values=[0]*len(data)
@@ -94,9 +73,3 @@ def preprocess(data,train=True):
                 values[subject_i]=1
         data_n[subcol]=values
     return data_n
-
-def getRevereseDict(d):
-    return dict(zip(
-        d.values(),d.keys())
-    )
-
