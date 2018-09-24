78859f8f50d374cd100b9409d10dbc2793144a04
SeenivasanSeeni <seeni0424@gmail.com>
2018-09-24 23:32:11 +0530

    done clean Driver

--- src/Driver.py
+++ src/Driver.py
@@ -1,67 +1,46 @@
-import handler
+import Handler
 import Preprocessor
 import Model
 
 def Train():
-    # initialize sources and models
-    csvhand = handler.CSVHandler("../DATA/input.csv")
-    csvhand.open()
+    """
+       Trains the model with data from DATA/input.csv
+    :return:
+    """
+    # initialize source -csv source
+    csvhand = Handler.CSVHandler("../DATA/input.csv")
     train_data = csvhand.getData()
-    labels = train_data["FI"]
     train_data_pro = Preprocessor.preprocess(train_data)
-    Model.Init()
+    Model.Init() #intialize the model
     labels_ = Model.Train(train_data_pro)
     return
 
 def Predict():
+    """
+        Predicts the data from DATA/test.csv
+    :return:
+    """
+
     #get user input from file test.csv
-    csvhand=handler.CSVHandler("../DATA/test.csv")
-    csvhand.open()
+    csvhand=Handler.CSVHandler("../DATA/test.csv")
     pred_data=csvhand.getData()
-    labels=pred_data["FI"]
     pred_data_pro=Preprocessor.preprocess(pred_data,train=False)
-    fieldsInData=pred_data_pro.columns
-    pred=Model.Predict(pred_data_pro)
-    C = Model.getClusterCenters()
-    for pre_i in range(len(pred)):
-        #get the cluster center for it
-        pre=pred[pre_i]
-        Ctarget=C[pre]
-        stu=pred_data_pro.values[pre_i]
+    fieldsInData=list(Preprocessor.getFI())+list(Preprocessor.getSubjects()) # fields are FI_X and S_X
+    predictions=Model.Predict(pred_data_pro)
+    C = Model.getClusterCenters() #cluster centers
+    for pred_i in range(len(predictions)):
+        pred=predictions[pred_i] #get the prediciton vlaue form the list
+        Ctarget=C[pred]  # get he corresponding cluster
+        student=pred_data_pro.values[pred_i] #get the row of values (for a student) for which predicition is done
         print("Needed ",end=" ")
         for att_i in range(len(fieldsInData)):
-            Catt=Ctarget[att_i]
-            stuatt=stu[att_i]
-            field=fieldsInData[att_i]
-            if(Catt>0.5):
-                print(field,end=" ")
+            Catt=Ctarget[att_i] #Cluster attribute value
+            studentAtt=student[att_i]  # attribute value present in thhe
+            field=fieldsInData[att_i]  # column name
+            if(Catt>0.5): #if the value is high . This might me changed later
+                print(field,end=" ") #print the field or append it to the result array
         print()
     return
-
-
-def testDrive():
-    #initialize sources and models
-    csvhand=handler.CSVHandler("../DATA/input.csv")
-    csvhand.open()
-    train_data=csvhand.getData()
-    labels=train_data["FI"]
-    train_data_pro=Preprocessor.preprocess(train_data)
-    Model.Init()
-    labels_=Model.Train(train_data_pro)
-    print(labels_)
-    print(labels.values)
-    print(len(train_data_pro.columns))
-    fields=train_data_pro.columns
-    columns_len=len(train_data_pro.columns)
-    input_stu=[0]*columns_len
-    input_stu[1]=1
-    C=Model.getClusterCenters()
-    pred=Model.Predict([input_stu])
-    predictedCluster=C[pred][0] #0 is due to pandas data storing format
-    for i in range(len(predictedCluster)):
-        value=predictedCluster[i]
-        if(value>0):
-            print("CLuster Value",value,"Column ",fields[i],"index",i)
 
 if __name__ == '__main__':
     Train()
