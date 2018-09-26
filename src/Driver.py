ff8d39694d291e9753f02cd2d052103edef95077
SeenivasanSeeni <seeni0424@gmail.com>
2018-09-26 10:31:05 +0530

    predictNExtSubject Done

--- src/Driver.py
+++ src/Driver.py
@@ -79,9 +79,54 @@ def PredictRaw(studentRecord):
     print()
     return result
 
+def getDifficulty():
+    return {
+        "ML":70,
+        "SC":80,
+        "DM":50,
+        "SA":40,
+        "DC":30,
+        "AE":12,
+        "CV":45,
+        "CN":67,
+        "OS":34,
+        "UI":45,
+        "SW":54,
+        "WT":67,
+        "CS":24,
+        "WS":12
+    }
+
+def PredictNextSubject(student_subjects,pred_subjects,debug=False):
+    difficulty=getDifficulty()
+    difficulty_subjects=[difficulty[sub] for sub in student_subjects if sub!=None]
+    difficulty_subjects.append(0)
+    maxDifficultyStudent=max(difficulty_subjects) # find the subjects which the student has taken for the field
+    #find the next smaller higher difficulty subject
+    nextDifficulty=1000
+    #highest Diffulty Subject
+    nextDifficultySubject=None
+    for sub in difficulty.keys():
+        diffi=difficulty[sub]
+        if( not sub in pred_subjects):
+            continue
+        if(diffi>maxDifficultyStudent): # check for subject is challenging enough
+            if(diffi<nextDifficulty):
+                nextDifficulty=diffi
+                nextDifficultySubject=sub
+    if(debug):
+        print("Difficulty of Subjects choosen by student {}".format([[sub, difficulty[sub]] for sub in student_subjects if sub!=None]))
+        print("Difficulty of Subjects predicted for student {}".format([[sub, difficulty[sub]] for sub in pred_subjects if sub!=None]))
+        print("Next Difficulty : {}  Subject: {}".format(nextDifficulty,nextDifficultySubject))
+    return nextDifficultySubject
+
+
 if __name__ == '__main__':
     Train()
     result=PredictRaw([1,"AI","OS","CN","CV"])
     print("OUTPUT:",result)
     result=PredictRaw([1,"AI",None,None,None])
     print("OUTPUT:",result)
+    nextSubject=PredictNextSubject(["SA",None,None],result["S"])
+    import pdb
+    pdb.set_trace()
