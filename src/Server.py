cc32062563bc6539c7c3f2aa205968507abe7a5a
SeenivasanSeeni <seeni0424@gmail.com>
2018-09-26 12:54:57 +0530

    done initial workking

---
+++ src/Server.py
@@ -0,0 +1,32 @@
+from flask import Flask,request
+import Driver
+
+
+def setup():
+    Driver.Train()
+    print("Training completed")
+    return
+
+app=Flask(__name__)
+
+@app.route("/predict",methods=["GET","POST"])
+def predictNextSubject():
+    if(request.method=="POST"):
+        try:
+            data=request.get_json();
+            student=[data["ID"],data["FI"]]
+            student.extend([s for s in data["S"] ]); #it should not contai any none and undefined
+            nextSubject=Driver.PredictForServer(student)
+            print("Next Subject",nextSubject)
+            return "{}".format(nextSubject)
+        except:
+            return 'Error'
+    return "Working"
+
+def setupDriver():
+    Driver.Train()
+
+
+if __name__ == '__main__':
+    setupDriver()
+    app.run(debug=True)
